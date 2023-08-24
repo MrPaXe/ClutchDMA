6af1914cc6c8147106819be40988cb28e1ea2674
Philipp <philipp.pausch@outlook.com>
2023-08-24 03:22:51 +0200

    fixed ftdinstall

--- dmaStuff/ftdDriverInstall.bat
+++ dmaStuff/ftdDriverInstall.bat
@@ -1,13 +1,3 @@
 ::Auto driver install for DMA USB
-pnputil /add-driver ".\FTD3XXDriver_WHQLCertified_v1.3.0.4\x64\Win10\FTDIBUS3.inf" >NUL
-
-if ERRORLEVEL 1 (
-	echo.
-	echo Failed to install USB driver.
-	echo.
-	goto end
-) ELSE (
-	echo.
-	echo USB driver successfully installed.
-	echo.
-)
+::/install very important
+pnputil /add-driver ".\FTD3XXDriver_v1.3.0.4\x64\Win10\FTDIBUS3.inf" /install
